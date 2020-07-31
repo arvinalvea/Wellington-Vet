18a9d18d129c0ec9b9c6f0ac1e2eb8c9824b146f
Arvin Alvea <arvinalvea@gmail.com>
2020-07-31 17:17:22 -0400

    Added bootstrap and navbar

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function stickyHeader() {
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  const x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
